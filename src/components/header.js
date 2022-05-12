a6d570543e48c41bbf9d7052245101763657edd5
Jacques Everiss <jacques.everiss@gmail.com>
2022-05-12 18:51:30 +0100

    added some styles and the desgin components and, still need to finish hooks

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import styles from './components-style/header.module.css'
+import React from "react";
+import styles from "./components-style/header.module.css";
 
 export default function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <div className={styles.label5} href="index.html"></div>
-          <div className={styles.label2} href="design.html"></div>
-          <div className={styles.label3} href="music.html"></div>
-          <div className={styles.label4} href="contact.html"></div>
-        </ul>
+      <nav className={styles.navigation}>
+        <div className={styles.label5} href="index.html"></div>
+        <div className={styles.label2} href="design.html"></div>
+        <div className={styles.label3} href="music.html"></div>
+        <div className={styles.label4} href="contact.html"></div>
       </nav>
     </header>
-  )
+  );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import Scroller from '../components/scroller'
-import pic from '../images/profile.jpg'
-import styles from './home.module.css'
+import React from "react";
+import Scroller from "../components/scroller";
+import pic from "../images/profile.jpg";
+import styles from "./home.module.css";
 
 export default function Home() {
   return (
-    <div>
-        <h1>Jacques Everiss</h1>
+    <div className={styles.homeGridParent}>
+      <h1 className={styles.homeH1}>
+        <div className={styles.homeGrid1}>
+          <div className={styles.firstName}>
+            <div className={styles.colourOne}>J</div>
+            <div className={styles.colourTwo}>a</div>
+            <div className={styles.colourThree}>c</div>
+            <div className={styles.colourFour}>q</div>
+            <div className={styles.colourThree}>u</div>
+            <div className={styles.colourTwo}>e</div>
+            <div className={styles.colourOne}>s</div>
+          </div>
+
+          <div className={styles.lastName}>
+            <div className={styles.colourFour}>E</div>
+            <div className={styles.colourThree}>v</div>
+            <div className={styles.colourTwo}>e</div>
+            <div className={styles.colourOne}>r</div>
+            <div className={styles.colourTwo}>i</div>
+            <div className={styles.colourThree}>s</div>
+            <div className={styles.colourFour}>s</div>
+          </div>
+        </div>
+      </h1>
+      <div className={styles.homeGrid2}>
         <img className={styles.me} src={pic} alt="Jacques Everiss"></img>
-        <Scroller/>
+      </div>
+      <div className={styles.homeGrid3}>
+        <Scroller />
+      </div>
     </div>
-  )
+  );
 }
